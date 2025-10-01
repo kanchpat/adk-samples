77861f217b4c7351aafb2c163c1e511ff793741e
Kanchana Patlolla <kpatlolla@google.com>
2025-10-01 23:48:07 +0000

    firebase

--- .firebase-studio/post-create.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "This is the post-create script from the ROOT template."
--- .idx/dev.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }: {
-  channel = "stable-23.11";
-  packages = [ pkgs.hello ];
-  startup = {
-    foreground = ''
-      echo "Root template test successful!"
-    '';
-  };
-}
